0a16a1a7caf00f4fa77748363711dff1864e3bd1
Ju <julien.hourcadette@live.fr>
2024-05-22 02:36:18 +0200

    init.sql: turn every function in procedure

--- compose/init.sql
+++ compose/init.sql
@@ -1,26 +1,107 @@
 CREATE TABLE files (
-    id SERIAL PRIMARY KEY,
-    name TEXT NOT NULL,
-    size int NOT NULL,
-    file_hash TEXT NOT NULL,
-    last_modified TIMESTAMP NOT NULL,
-    misnamed_score CHAR(1) NOT NULL,
-    perished_score CHAR(1) NOT NULL,
-    duplicated_score CHAR(1) NOT NULL,
-    global_score CHAR(1) NOT NULL
+                       id SERIAL PRIMARY KEY,
+                       name TEXT NOT NULL,
+                       size int NOT NULL,
+                       file_hash TEXT NOT NULL,
+                       last_modified TIMESTAMP NOT NULL,
+                       misnamed_score CHAR(1) NOT NULL,
+                       perished_score CHAR(1) NOT NULL,
+                       duplicated_score CHAR(1) NOT NULL,
+                       global_score CHAR(1) NOT NULL
 );
 
 CREATE TABLE duplicate_associative_table (
-    id SERIAL PRIMARY KEY,
-    original_file_id INTEGER NOT NULL,
-    duplicate_file_id INTEGER NOT NULL,
-    CONSTRAINT fk_original_file
-      FOREIGN KEY (original_file_id)
-      REFERENCES files (id),
-    CONSTRAINT fk_duplicate_file
-      FOREIGN KEY (duplicate_file_id)
-      REFERENCES files (id)
+                                             id SERIAL PRIMARY KEY,
+                                             original_file_id INTEGER NOT NULL,
+                                             duplicate_file_id INTEGER NOT NULL,
+                                             CONSTRAINT fk_original_file
+                                                 FOREIGN KEY (original_file_id)
+                                                     REFERENCES files (id),
+                                             CONSTRAINT fk_duplicate_file
+                                                 FOREIGN KEY (duplicate_file_id)
+                                                     REFERENCES files (id)
 );
+
+CREATE TABLE rules (
+                       id SERIAL PRIMARY KEY,
+                       name TEXT NOT NULL,
+                       description TEXT,
+                       weight FLOAT NOT NULL,
+                       rules_config JSON NOT NULL
+);
+
+INSERT INTO rules (name, description, weight, rules_config)
+VALUES
+    (
+        'misnamed',
+        'Filename must follow specific naming conventions',
+        3.0,
+        '{
+          "type": "misnamed",
+          "regex_rules": [
+            {
+              "name": "Date",
+              "description": "Filename must contain a date with format ..._yyyy, ex: phone_bill_robert_2024.pdf",
+              "regex": "_[0-9]{4}\\.",
+              "weight": 3
+            },
+            {
+              "name": "4 separator",
+              "description": "Filename must have 4 separators which are underscore only, ex: phone_bill_robert_2024.pdf",
+              "regex": "^[^_]*(_[^_]*){3}$",
+              "weight": 1.8
+            },
+            {
+              "name": "3 words",
+              "description": "Filename must contain 3 words separated by underscore, ex: phone_bill_robert_2024.pdf",
+              "regex": "^\\w+_\\w+_\\w+_.+$",
+              "weight": 3
+            },
+            {
+              "name": "Extension",
+              "description": "Filename must contain an extension, ex: phone_bill_robert_2024.pdf",
+              "regex": "\\.\\w+$",
+              "weight": 2.5
+            },
+            {
+              "name": "White Space",
+              "description": "Don''t accept white spaces",
+              "regex": "^\\S+$",
+              "weight": 2
+            },
+            {
+              "name": "Unauthorized Char",
+              "description": "Don''t accept special characters",
+              "regex": "^[A-Za-z0-9._]*$",
+              "weight": 2
+            }
+          ]
+        }'
+    );
+
+
+INSERT INTO rules (name, description, weight, rules_config)
+VALUES
+    ('perished',
+     'File is considered perished if it has not been modified for a certain period',
+     2.0,
+     '{
+       "type": "perished",
+       "expiration_days": 300
+     }'
+    );
+
+INSERT INTO rules (name, description, weight, rules_config)
+VALUES
+    ('duplicated',
+     'File is considered duplicated if it appears too many times in the system',
+     1.5,
+     '{
+       "type": "duplicated",
+       "max_occurrences": 3
+     }'
+    );
+
 
 -- TEST VALUES SHOULD BE DELETE
 INSERT INTO files (name, size, file_hash, last_modified, misnamed_score, perished_score, duplicated_score, global_score)
@@ -35,31 +116,45 @@ VALUES
     ('correct_perished_mot2_2010.txt', 2025, 'acd', '2023-02-05', 'N', 'N', 'N', 'N'),
     ('bad! ', 2025, 'abcd', '2023-02-05', 'N', 'N', 'N', 'N'),
     ('special_aze_&ù\_2022.txt', 2025, 'abcd', '2023-02-05', 'N', 'N', 'N', 'N');
+-------------------------------------------------
 
 ALTER TABLE duplicate_associative_table
-ADD CONSTRAINT unique_file_pair UNIQUE (original_file_id, duplicate_file_id);
+    ADD CONSTRAINT unique_file_pair UNIQUE (original_file_id, duplicate_file_id);
 
 ALTER TABLE duplicate_associative_table
-ADD CONSTRAINT no_self_duplicate CHECK (original_file_id != duplicate_file_id);
+    ADD CONSTRAINT no_self_duplicate CHECK (original_file_id != duplicate_file_id);
 
-CREATE OR REPLACE FUNCTION detect_and_store_duplicates() RETURNS VOID AS $$
+CREATE OR REPLACE PROCEDURE detect_and_store_duplicates()
+    LANGUAGE plpgsql AS $$
 BEGIN
     INSERT INTO duplicate_associative_table (original_file_id, duplicate_file_id)
     SELECT f1.id, f2.id
     FROM files f1
-    JOIN files f2 ON f1.file_hash = f2.file_hash AND f1.id < f2.id
+             JOIN files f2 ON f1.file_hash = f2.file_hash AND f1.id < f2.id
     ON CONFLICT DO NOTHING;
 END;
-$$ LANGUAGE plpgsql;
+$$;
 
-CREATE OR REPLACE FUNCTION calculate_perished_score(file_id INT)
-RETURNS VOID AS $$
+
+CREATE OR REPLACE PROCEDURE calculate_perished_score(file_id INT)
+    LANGUAGE plpgsql AS $$
 DECLARE
     calculated_score CHAR(1);
     res INTERVAL;
-    day_duration_limit INT := 300; -- Perished limit, ten months for the example
+    day_duration_limit INT;
     file_path TEXT;
+    rule_config JSONB;
 BEGIN
+    SELECT rules_config INTO rule_config
+    FROM rules
+    WHERE name = 'perished';
+
+    BEGIN
+        day_duration_limit := (rule_config->>'expiration_days')::INT;
+    EXCEPTION WHEN others THEN
+        day_duration_limit := 300;
+    END;
+
     SELECT NOW() - last_modified, name INTO res, file_path
     FROM files
     WHERE id = file_id;
@@ -84,44 +179,45 @@ BEGIN
     SET perished_score = calculated_score
     WHERE id = file_id;
 END;
-$$ LANGUAGE plpgsql;
+$$;
 
-
-CREATE OR REPLACE FUNCTION calculate_every_perished_scores()
-RETURNS VOID AS $$
-DECLARE
-    file_record RECORD;
-BEGIN
-    FOR file_record IN SELECT id FROM files LOOP
-        PERFORM calculate_perished_score(file_record.id);
-    END LOOP;
-END;
-$$ LANGUAGE plpgsql;
 
 CREATE OR REPLACE FUNCTION assign_duplicated_grade(occurrences INT)
-RETURNS CHAR(1) AS $$
+    RETURNS CHAR(1) AS $$
 BEGIN
     RETURN CASE
-        WHEN occurrences < 1 THEN 'A'
-        WHEN occurrences <= 1 THEN 'B'
-        WHEN occurrences <= 3 THEN 'C'
-        WHEN occurrences <= 5 THEN 'D'
-        WHEN occurrences <= 8 THEN 'E'
-        ELSE 'F'
-    END;
+               WHEN occurrences < 1 THEN 'A'
+               WHEN occurrences <= 1 THEN 'B'
+               WHEN occurrences <= 3 THEN 'C'
+               WHEN occurrences <= 5 THEN 'D'
+               WHEN occurrences <= 8 THEN 'E'
+               ELSE 'F'
+        END;
 END;
 $$ LANGUAGE plpgsql;
 
-CREATE OR REPLACE FUNCTION calculate_duplicated_score(file_id INT)
-RETURNS VOID AS $$
+CREATE OR REPLACE PROCEDURE calculate_duplicated_score(file_id INT)
+    LANGUAGE plpgsql AS $$
 DECLARE
     occurrence_count INT;
     new_score CHAR(1);
+    max_occurrences INT;
+    rule_config JSONB;
 BEGIN
+    SELECT rules_config INTO rule_config
+    FROM rules
+    WHERE name = 'duplicated';
+
+    BEGIN
+        max_occurrences := (rule_config->>'max_occurrences')::INT;
+    EXCEPTION WHEN others THEN
+        max_occurrences := 5;
+    END;
+
     SELECT COUNT(*) INTO occurrence_count
     FROM files
     WHERE file_hash = (SELECT file_hash FROM files WHERE id = file_id)
-    AND id != file_id;
+      AND id != file_id;
 
     new_score := assign_duplicated_grade(occurrence_count);
 
@@ -131,17 +227,101 @@ BEGIN
 
     RAISE NOTICE 'File ID: % has duplicated score: % with % occurrences', file_id, new_score, occurrence_count;
 END;
+$$;
+
+
+CREATE OR REPLACE FUNCTION assign_misnamed_grade(score FLOAT)
+    RETURNS TEXT AS $$
+BEGIN
+    IF score >= 0.9 THEN
+        RETURN 'A';
+    ELSIF score >= 0.8 THEN
+        RETURN 'B';
+    ELSIF score >= 0.7 THEN
+        RETURN 'C';
+    ELSIF score >= 0.6 THEN
+        RETURN 'D';
+    ELSIF score >= 0.5 THEN
+        RETURN 'E';
+    ELSE
+        RETURN 'F';
+    END IF;
+END;
 $$ LANGUAGE plpgsql;
 
 
+CREATE OR REPLACE PROCEDURE calculate_misnamed_score(file_id INT)
+    LANGUAGE plpgsql AS $$
+DECLARE
+    res FLOAT := 0;
+    total_rule_count INT;
+    tidy_score_decimal FLOAT;
+    misnamed_grade TEXT;
+    rule_record JSONB;
+    rule_weight FLOAT;
+    rule_regex TEXT;
+    rule_name TEXT;
+    file_name TEXT;
+BEGIN
+    SELECT name INTO file_name FROM files WHERE id = file_id;
 
-CREATE OR REPLACE FUNCTION calculate_every_duplicated_scores()
-RETURNS VOID AS $$
+    SELECT rules_config INTO rule_record FROM rules WHERE name = 'misnamed';
+
+    total_rule_count := jsonb_array_length(rule_record->'regex_rules');
+
+    RAISE INFO 'Applying MISNAMED Rules...';
+
+    FOR i IN 0..(total_rule_count - 1) LOOP
+            rule_name := rule_record->'regex_rules'->i->>'name';
+            rule_regex := rule_record->'regex_rules'->i->>'regex';
+            rule_weight := (rule_record->'regex_rules'->i->>'weight')::FLOAT;
+
+            IF (SELECT regexp_matches(file_name, rule_regex) IS NOT NULL) THEN
+                RAISE INFO 'File : % match the rule : %', file_name, rule_name;
+                res := res + rule_weight;
+            ELSE
+                RAISE INFO 'File : % doesn''t match the rule : %', file_name, rule_name;
+            END IF;
+        END LOOP;
+
+    tidy_score_decimal := res / total_rule_count;
+    misnamed_grade := assign_misnamed_grade(tidy_score_decimal);
+
+    UPDATE files SET misnamed_score = misnamed_grade WHERE id = file_id;
+
+    RAISE INFO 'Misnamed grade for file %: %', file_id, misnamed_grade;
+END;
+$$;
+
+CREATE OR REPLACE PROCEDURE calculate_every_duplicated_scores()
+    LANGUAGE plpgsql AS $$
 DECLARE
     file_record RECORD;
 BEGIN
     FOR file_record IN SELECT id FROM files LOOP
-        PERFORM calculate_duplicated_score(file_record.id);
-    END LOOP;
+            PERFORM calculate_duplicated_score(file_record.id);
+        END LOOP;
 END;
-$$ LANGUAGE plpgsql;
+$$;
+
+CREATE OR REPLACE PROCEDURE calculate_every_perished_scores()
+    LANGUAGE plpgsql AS $$
+DECLARE
+    file_record RECORD;
+BEGIN
+    FOR file_record IN SELECT id FROM files LOOP
+            PERFORM calculate_perished_score(file_record.id);
+        END LOOP;
+END;
+$$;
+
+CREATE OR REPLACE PROCEDURE calculate_every_misnamed_scores()
+    LANGUAGE plpgsql AS $$
+DECLARE
+    file_record RECORD;
+BEGIN
+    FOR file_record IN SELECT id FROM files LOOP
+            PERFORM calculate_misnamed_score(file_record.id);
+        END LOOP;
+END;
+$$;
