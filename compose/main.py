ba7a8cd1e413daa10b4b5b8b4f750ac068661c5b
GuyomTr <guillaume.terriere@epitech.eu>
2025-01-24 10:33:36 +0100

    change all date to less than 1 year

--- compose/main.py
+++ compose/main.py
@@ -15,53 +15,53 @@ def create_challenge_files(base_path):
     # Fichiers mal nommés
     misnamed_files = [
         ("projet-secret_marc.pdf", "mauvais séparateur", "2024-03-01"),
-        ("photomariage.pdf", "petit piege de fichier dupliqué qui ne l'est pas", "2023-02-15"),
+        ("photomariage.pdf", "petit piege de fichier dupliqué qui ne l'est pas", "2024-02-15"),
         ("c@r@ctere spec!aux.docx",
         "Fichier contenant des caractères spéciaux comme %, @ ou !.", "2024-03-10"),
         ("sans_extension", "Document sans extension, difficile à ouvrir.", "2024-01-20"),
         ("Résumé_reunion1012_2021.docx",
         "Résumé de données financières, avec des erreurs dans le nom.", "2021-04-01"),
         ("backup_db!.txt",
-         "Sauvegarde avec des caractères illégaux dans le titre.", "2023-03-25"),
-        ("photos été.jpg", "Une photo mal nommée, mélange d'espaces et de caractères spéciaux.", "2023-02-10"),
-        ("Doc1", "Document inutile et très mal nommé.", "2023-03-05"),
-        ("Doc2", "Document inutile et très mal nommé.", "2023-02-20"),
-        ("Doc3", "Document inutile et très mal nommé.", "2023-01-15"),
-        ("Doc4", "Document inutile et très mal nommé.", "2023-04-10"),
-        ("Doc5", "Document inutile et très mal nommé.", "2023-03-30"),
-        ("Doc6", "Document inutile et très mal nommé.", "2023-01-25"),
-        ("Doc7", "Document inutile et très mal nommé.", "2023-04-05"),
-        ("Doc8", "Document inutile et très mal nommé.", "2023-02-15"),
-        ("Doc9", "Document inutile et très mal nommé.", "2023-01-10"),
-        ("Doc10", "Document inutile et très mal nommé.", "2023-03-20"),
-        ("Doc11", "Document inutile et très mal nommé.", "2023-03-05"),
-        ("Doc12", "Document inutile et très mal nommé.", "2023-02-25"),
-        ("Doc13", "Document inutile et très mal nommé.", "2023-01-05"),
-        ("Doc14", "Document inutile et très mal nommé.", "2023-03-01"),
-        ("Doc15", "Document inutile et très mal nommé.", "2023-02-10"),
-        ("Doc16", "Document inutile et très mal nommé.", "2023-01-20"),
-        ("Doc17", "Document inutile et très mal nommé.", "2023-04-01"),
-        ("Doc18", "Document inutile et très mal nommé.", "2023-03-15"),
+         "Sauvegarde avec des caractères illégaux dans le titre.", "2024-03-25"),
+        ("photos été.jpg", "Une photo mal nommée, mélange d'espaces et de caractères spéciaux.", "2024-02-10"),
+        ("Doc1", "Document inutile et très mal nommé.", "2024-03-05"),
+        ("Doc2", "Document inutile et très mal nommé.", "2024-02-20"),
+        ("Doc3", "Document inutile et très mal nommé.", "2024-01-15"),
+        ("Doc4", "Document inutile et très mal nommé.", "2024-04-10"),
+        ("Doc5", "Document inutile et très mal nommé.", "2024-03-30"),
+        ("Doc6", "Document inutile et très mal nommé.", "2024-01-25"),
+        ("Doc7", "Document inutile et très mal nommé.", "2024-04-05"),
+        ("Doc8", "Document inutile et très mal nommé.", "2024-02-15"),
+        ("Doc9", "Document inutile et très mal nommé.", "2024-01-10"),
+        ("Doc10", "Document inutile et très mal nommé.", "2024-03-20"),
+        ("Doc11", "Document inutile et très mal nommé.", "2024-03-05"),
+        ("Doc12", "Document inutile et très mal nommé.", "2024-02-25"),
+        ("Doc13", "Document inutile et très mal nommé.", "2024-01-05"),
+        ("Doc14", "Document inutile et très mal nommé.", "2024-03-01"),
+        ("Doc15", "Document inutile et très mal nommé.", "2024-02-10"),
+        ("Doc16", "Document inutile et très mal nommé.", "2024-01-20"),
+        ("Doc17", "Document inutile et très mal nommé.", "2024-04-01"),
+        ("Doc18", "Document inutile et très mal nommé.", "2024-03-15"),
     ]
 
 
     correct_files = [
-        ("facture_achats_mars_2023.pdf",
-        "Facture d'achats pour le mois de mars 2023.", "2023-12-01"),
+        ("facture_achats_mars_2024.pdf",
+        "Facture d'achats pour le mois de mars 2024.", "2024-12-01"),
         ("rapport_activite_2022.docx",
         "Rapport complet des activités de l'année 2022.", "2024-11-25"),
         ("planning_reunion_equipe_2024.xlsx",
         "Planning de réunion d'équipe pour l'année 2024.", "2025-01-10"),
-        ("notes_projet_alpha_2023.txt",
-        "Notes sur le projet Alpha rédigées en 2023.", "2024-10-20"),
+        ("notes_projet_alpha_2024.txt",
+        "Notes sur le projet Alpha rédigées en 2024.", "2024-10-20"),
         ("liste_clients_premium_2024.csv",
         "Liste des clients premium avec leurs informations principales.", "2024-12-15"),
         ("budget_previsionnel_2025.pdf",
         "Document détaillant le budget prévisionnel de l'année 2024.", "2025-01-05"),
-        ("proposition_commerciale_2023.pdf",
-        "Proposition commerciale dupliquée, même contenu sur 2 fichiers.", "2023-12-25"),
-        ("proposition_commerciale_2023 copie.pdf",
-        "Proposition commerciale dupliquée, même contenu sur 2 fichiers.", "2023-12-25"),
+        ("proposition_commerciale_2024.pdf",
+        "Proposition commerciale dupliquée, même contenu sur 2 fichiers.", "2024-12-25"),
+        ("proposition_commerciale_2024 copie.pdf",
+        "Proposition commerciale dupliquée, même contenu sur 2 fichiers.", "2024-12-25"),
     ]
 
     # Créer les fichiers mal nommés
@@ -82,7 +82,7 @@ def create_file(folder_path, file_name, content, custom_date):
     set_file_timestamps(file_path, custom_date)
 
 
-def set_file_timestamps(file_path, custom_date="2022-01-01"):
+def set_file_timestamps(file_path, custom_date="2025-01-01"):
     """
     Modifie les dates de dernière modification et d'accès du fichier.
     """
